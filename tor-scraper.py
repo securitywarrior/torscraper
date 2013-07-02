d9bb1e02818fe97f0c8fbcad0add107f9d37abe7
benjkelley <benjkelley@gmail.com>
2013-07-02 10:35:06 -0400

    Switched from lists to queues

--- tor-scraper.py
+++ tor-scraper.py
@@ -7,12 +7,14 @@ import http
 import re
 import argparse
 import datetime
+import queue
 
 import socks		# Now using branch version for compatibility with Python 3.x
 			# https://code.google.com/p/socksipy-branch/
 
 import stem		# http://stem.torproject.org
 import stem.process
+import stem.control
 from stem.util import term
 
 import couchdb		# Running branch patched for Python 3
@@ -26,12 +28,12 @@ from couchdb.mapping import Document, TextField, IntegerField, DateTimeField
 DEBUG = True
 
 def query(url):
-	#try:
-	opener = urllib.request.build_opener(SocksiPyHandler(socks.PROXY_TYPE_SOCKS5, "localhost", 9050))
-	h = opener.open(url)
-	return h
-	#except:
-		#print("[E] Unable to return {0}".format(url))
+	try:
+		opener = urllib.request.build_opener(SocksiPyHandler(socks.PROXY_TYPE_SOCKS5, "localhost", 9050))
+		h = opener.open(url)
+		return h
+	except:
+		print("[E] Unable to return {0}".format(url))
 
 class SocksiPyConnection(http.client.HTTPConnection):
     def __init__(self, proxytype, proxyaddr, proxyport = None, rdns = True, username = None, password = None, *args, **kwargs):
@@ -71,7 +73,7 @@ def print_bootstrap_lines(line):
 		print(term.format(line, term.Color.BLUE))
 
 def check_http(url):
-	# urllib2 needs URLs to include http://, so if the user submits "example.com" this will prepend http://
+	# urllib needs URLs to include http://, so if the user submits "example.com" this will prepend http://
 	if not re.search("^http[s]?\:\/\/", url):
 		url = "http://" + url
 
@@ -85,25 +87,7 @@ def get_title(lines):
 			title = m.group(0) 
 	return title[7:-8]
 
-
-def main():
-
-	# TODO: Add command line switch to set db server? Or config file?
-	dbserv = couchdb.Server('http://localhost:5984/')
-
-	# CouchDB connection and db creation
-	try:
-		db = dbserv['sites']
-	except socket.error as e:
-		print("[E] Could not connect to the database!")
-		if DEBUG:
-			print("Error message: {0}".format(e))
-		sys.exit("Please make sure that the database has been started, and try again")
-	except couchdb.http.ResourceNotFound:
-		if DEBUG:
-			print("[I] Building initial database...")
-		db = dbserv.create('sites')
-
+def start_tor():
 	print(term.format("Starting Tor:\n", term.Attr.BOLD))
 	try:
 		tor_process = stem.process.launch_tor_with_config(
@@ -124,11 +108,67 @@ def main():
 			print("Error message: {0}".format(e))
 		# TODO: Remove this, make script try to connect to running instance
 		sys.exit("Please kill all running Tor instances and try again")
-			
 
-	domains = []
+def scrape_site(site, domains, db):
+	if DEBUG:
+		print(term.format("Scraping %s\n" % site, term.Attr.BOLD))
+
+	try:
+		lines = query(site).readlines()
+
+	except AttributeError as e:
+		print("[E] Site could not be read!")
+		if DEBUG:
+			print("Error message: {0}".format(e))
+		lines = []
+
+	lines = [x.decode('utf8', errors='replace') for x in lines]
+
+	#Update the current site's entry
+	doc = DB_Structure.load(db, check_http(site))
+	doc.LastAccessed = datetime.datetime.now()
+	doc.title = get_title(lines)
+	doc.store(db)
+
+	for x in lines:
+		m = re.search('\w+\.onion', x)
+		if m:
+			full_url = check_http(m.group(0))
+			domains.put(full_url)
+			if full_url not in db:
+				current_time = datetime.datetime.now()
+				new_site = DB_Structure(_id = full_url, url = full_url, ref = site, Discovered=current_time, LastAccessed=current_time, title='')
+				new_site.store(db)
+
+def main():
+
+	# TODO: Add command line switch to set db server? Or config file?
+	dbserv = couchdb.Server('http://localhost:5984/')
+
+	# CouchDB connection and db creation
+	try:
+		db = dbserv['sites']
+	except socket.error as e:
+		print("[E] Could not connect to the database!")
+		if DEBUG:
+			print("Error message: {0}".format(e))
+		sys.exit("Please make sure that the database has been started, and try again")
+	except couchdb.http.ResourceNotFound:
+		if DEBUG:
+			print("[I] Building initial database...")
+		db = dbserv.create('sites')
+
+	# start the Tor process
+	start_tor()
+	control = stem.control.Controller.from_port(address='127.0.0.1', port=9051)
+	control.authenticate()
+	control.new_circuit(path=None, purpose='general', await_build=False)
+	print(control.get_info('circuit-status'))
+
+		
+	domains = queue.Queue(0)
 	url = check_http(args.url)
-	domains.append(url)
+	domains.put(url)
 
 	# Prints endpoint information if debugging is enabled
 	if DEBUG:
@@ -137,8 +177,7 @@ def main():
 
 	print(term.format("\nScraping for .ONION domains:\n", term.Attr.BOLD))
 
-	# Main scraping loop
-	# Gathers domains into the database, and continues to scrape through each subsequent domain
+	# Sets up DB entry for initial site being scraped
 	if url not in db:	
 		prev_site = 'N/A - Starting Domain'
 		current_time = datetime.datetime.now()
@@ -146,36 +185,11 @@ def main():
 			Discovered=current_time, LastAccessed=current_time, title='')
 		urldoc.store(db)
 
-	for site in domains:
-		if DEBUG:
-			print(term.format("Scraping %s\n" % site, term.Attr.BOLD))
-
-		try:
-			lines = query(site).readlines()
-
-		except AttributeError as e:
-			print("[E] Site could not be read!")
-			if DEBUG:
-				print("Error message: {0}".format(e))
-			lines = []
-
-		lines = [x.decode('utf8', errors='replace') for x in lines]
-
-		#Update the current site's entry
-		doc = DB_Structure.load(db, check_http(site))
-		doc.LastAccessed = datetime.datetime.now()
-		doc.title = get_title(lines)
-		doc.store(db)
-
-		for x in lines:
-			m = re.search('\w+\.onion', x)
-			if m:
-				full_url = check_http(m.group(0))
-				domains.append(full_url)
-				if full_url not in db:
-					current_time = datetime.datetime.now()
-					new_site = DB_Structure(_id = full_url, url = full_url, ref = site, Discovered=current_time, LastAccessed=current_time, title='')
-					new_site.store(db)
+	# Main scraping loop
+	# Gathers domains into the database, and continues to scrape through each subsequent domain
+	while domains.qsize() > 0:
+		site = domains.get()
+		scrape_site(site, domains, db)
 	
 	print(term.format("\nScraping Complete.", term.Attr.BOLD))
 	tor_process.kill()
